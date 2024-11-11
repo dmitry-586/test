749cbf52f6a7ee30ff18b88a7645dcb46a73eb4f
Dmitry Kutsevalov <kutdmitry@gmail.com>
2024-11-11 18:06:18 +0300

    test 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Head from "next/head"; // Импортируем Head
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script src="//teleton.me/tools/chat.js" async></script>
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
