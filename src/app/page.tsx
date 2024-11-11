9e2d5c2d38e73f280ae01be348882adafb20fd43
Dmitry Kutsevalov <kutdmitry@gmail.com>
2024-11-11 18:19:30 +0300

    test 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Script from "next/script";
 export default function Home() {
-  <script src="//teleton.me/tools/chat.js" async></script>;
   return (
-    <div className="flex w-screen h-screen justify-center items-center">
-      <p>Hello world!</p>
-    </div>
+    <>
+      <Script src="//teleton.me/tools/chat.js" />
+      <div className="flex w-screen h-screen justify-center items-center">
+        <p>Hello world!</p>
+      </div>
+    </>
   );
 }
